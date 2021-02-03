fe7ab580de19a7d584bb8fac74e338b2cfbe4647
Anna Piechowski <apiechowski@convoy.com>
2021-02-02 21:47:23 -0800

    Add support for faux-dynamic header content; add support for custom scripts and SASS to theme

---
+++ assets/js/hero.js
@@ -0,0 +1,45 @@
+function displayHeroContent() {
+  var blockToShow;
+  var heroBlocks = $(".hero-announcement-content-block");
+  console.log(heroBlocks)
+
+  for (var block of heroBlocks) {
+    if (shouldShowBlock(block)) {
+      blockToShow = block;
+      break;
+    }
+  }
+
+  if (blockToShow) {
+    heroBlocks.not(blockToShow).remove();
+    $(blockToShow).addClass("the-chosen-one");
+  }
+}
+
+function shouldShowBlock(block) {
+  switch ($(block).attr("id")) {
+    case "latest-announcement":
+      return shouldShowLatestAnnouncement(block)
+    case "default":
+      return true;
+    default:
+      return false;
+  }
+}
+
+function shouldShowLatestAnnouncement(latestAnnouncement) {
+  var dateData = $(latestAnnouncement).data("date");
+
+  if (!dateData) {
+    return false;
+  }
+
+  var announcementDate = new Date(dateData)
+  var daysSinceAnnouncement = Math.floor(((new Date() - announcementDate) / 1000) / (60 * 60 *24));
+
+  return daysSinceAnnouncement <= 10;
+}
+
+$(document).ready(function() {
+  displayHeroContent();
+});
--- content/news/2021-01-31-announcing-2021-schedule/index.md
+++
@@ -1,6 +0,0 @@
----
-title   : "2021 Schedule Announced"
-date    : 2021-01-31T14:45:00-8:00
-draft   : false
-description: "2021 Schedule Announced"
----
