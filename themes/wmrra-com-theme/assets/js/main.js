0eafc08d1f107b7a8082b6f89b7559a832623c57
Anna Piechowski <anna@apiechowski.com>
2021-02-07 22:30:22 -0800

    Improve header menu appearance

--- themes/wmrra-com-theme/assets/js/main.js
+++ themes/wmrra-com-theme/assets/js/main.js
@@ -5,9 +5,10 @@ function activateHeaderMenu() {
   $(".header-menu > .header-menu-item").hover(function() {
     clearTimeout(submenuTimeout);
     $(".header-menu-item-submenu").hide();
-    $(this).children(".header-menu-item-submenu").show();
+    var submenu = $(this).children(".header-menu-item-label").children(".header-menu-item-submenu");
+    showSubmenu(submenu);
   }, function() {
-    hideSubmenuIfInactive($(this).children(".header-menu-item-submenu"));
+    hideSubmenuIfInactive($(this).children(".header-menu-item-submenu").children(".header-menu-item-submenu"));
   });
 
   $(".header-menu-item-submenu").hover(function() {
@@ -23,8 +24,33 @@ function hideSubmenuIfInactive(submenu) {
   submenuTimeout = setTimeout(function() {
     if(!submenuActive) {
       $(submenu).hide();
+      $(submenu).css("left", "-10px");
     }
   }, 500);
+}
+
+function showSubmenu(submenu) {
+  // if the menu is going to be off screen, we'll nudge it to the left
+  var menu = $(submenu);
+
+  // so gross, but we need to show the menu ever so 
+  // briefly to figure out where it is; the screen
+  // shouldn't repaint during this time, so users
+  // won't see a flicker (according to the internet)
+  menu.show()
+  var menuLeft = menu.offset().left;
+  menu.hide()
+  
+  var menuWidth = menu.outerWidth();
+  var windowWidth = $(window).width();
+  var menuRight = menuLeft + menuWidth;
+
+  if (menuRight > windowWidth) {
+    var newLeft = ((menuRight - windowWidth)  * -1) - 15;
+    menu.css("left", `${newLeft}px`);
+  }
+
+  menu.show();
 }
 
 $(document).ready(function() {
