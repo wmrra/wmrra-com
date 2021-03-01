95722e679f5dd816eb59b374a35927df7ae2b76c
Anna Piechowski <apiechowski@convoy.com>
2021-03-01 00:30:23 -0800

    Completed become-a-sponsor page w/ dynamic modal

--- assets/js/supporter-levels.js
+++ assets/js/supporter-levels.js
@@ -1,5 +1,21 @@
 function activateModalLinks() {
-  console.log("hi mom!")
+  var supporterLevelModal = $(".supporter-level-benefits-modal");
+  $(".see-all-benefits-link").click(function(event) {
+    event.preventDefault();
+    
+    var contentId = $(this).data("level-id");
+    $(".modal-overlay").removeClass("hidden");
+    $("html, body").addClass("modal-open");
+    supporterLevelModal.show();
+    supporterLevelModal.children(`#${contentId}`).removeClass("hidden");
+  });
+
+  supporterLevelModal.children(".modal-close-icon").click(function(){
+    supporterLevelModal.children(".benefits-modal-content:visible").addClass("hidden");
+    supporterLevelModal.hide();
+    $(".modal-overlay").addClass("hidden");
+    $("html, body").removeClass("modal-open");
+  });
 }
 
 $(document).ready(function() {
